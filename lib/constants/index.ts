dcd0c509dd4f8fbc857debbe74cfca1487d3663e
Anil kothapalli <anilkumar1988@gmail.com>
2025-08-14 15:41:19 +0200

    app url

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,3 +1,5 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Fleurs";
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Next gen store";
+export const APP_SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
