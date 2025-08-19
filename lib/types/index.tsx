1ac81bdc45128a7a773856a3f16205865fd28515
Anil kothapalli <anilkumar1988@gmail.com>
2025-08-19 15:33:47 +0200

    sample data and types

---
+++ lib/types/index.tsx
@@ -0,0 +1,20 @@
+type Product = {
+    name: string;
+    slug: string;
+    category: string;
+    description: string;
+    images: string[];
+    price: number;
+    brand: string;
+    rating: number;
+    numReviews: number;
+    stock: number;
+    isFeatured: boolean;
+    banner: string | null;
+};
+
+type ProductListProps = {
+    data: Product[];
+    title?: string;
+    limit?: number;
+};
