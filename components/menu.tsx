ee23f15c10d6ca44606dd63a6c16aa5bc786e092
Anil kothapalli <anilkumar1988@gmail.com>
2025-08-19 14:20:45 +0200

    implemented menu using sheet for small screens, its hidden fr md and above

--- components/menu.tsx
+++ components/menu.tsx
@@ -1,14 +1,15 @@
-import { ShoppingCartIcon, UserIcon } from "lucide-react";
+import { EllipsisVertical, MenuIcon, ShoppingCartIcon, UserIcon } from "lucide-react";
 import Link from "next/link";
 
 import ThemeToggle from "@/components/theme-toggle";
 import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 
 
 const Menu = () => {
-    return (<div className="flex-end">
+    return (<div className="flex justify-end gap-3">
 
-        <nav className="hidden md:inline-block w-full" >
+        <nav className="hidden md:flex w-full gap-1" >
             <ThemeToggle />
 
             <Button asChild variant="ghost">
@@ -17,6 +18,29 @@ const Menu = () => {
             <Button asChild >
                 <Link href="/login"> <UserIcon /> Login</Link>
             </Button>
+        </nav>
+
+        <nav className="md:hidden">
+
+            <Sheet>
+                <SheetTrigger className="align-middle">
+
+                    <EllipsisVertical />
+                </SheetTrigger>
+
+                <SheetContent className="flex flex-col items-start p-5">
+                    <SheetTitle>Menu</SheetTitle>
+
+                    <ThemeToggle />
+                    <Button asChild variant="ghost">
+                        <Link href="/cart"> <ShoppingCartIcon /> Cart</Link>
+                    </Button>
+                    <Button asChild >
+                        <Link href="/login"> <UserIcon /> Login</Link>
+                    </Button>
+                    <SheetDescription></SheetDescription>
+                </SheetContent>
+            </Sheet>
         </nav>
 
     </div>);
